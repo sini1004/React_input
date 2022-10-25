41f8f18c416f35e883247d418a45b270d6567307
shinhee <tlsgml656@gmail.com>
2022-10-25 12:44:25 +0900

    입력, 초기화 버튼

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Input = () => {
+  const [ text, setText ] = useState('');
+
+  const onChange = (e) => { //input에 입력할때마다 발생하는 함수
+    setText(e.target.value);
+  }
+  const onReset = () => {
+    setText ('')
+  }
   return (
     <div>
-      <input value='입력' type="text" />
-      <button>초기화</button>
+      <input onChange={onChange} value={text}  />
+      <button onClick={onReset}>초기화</button>
       <div>
-        입력값 : test
+        입력값 : {text}
       </div>
     </div>
   )
 }
 
-export default Input
+export default Input;
