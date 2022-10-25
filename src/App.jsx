85d7ed582c642af26b08f73ba5e3629d6bdbdf67
shinhee <tlsgml656@gmail.com>
2022-10-25 12:11:44 +0900

    App.jsx 파일 수정, Input.jsx input 코드 삽입

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Input from '/.Input.jsx';
+import Input from './Input';
 
 function App() {
   return (
     <>
-    test
+    <Input/>
     </>
   );
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div>
+      <input value='입력' type="text" />
+      <button>초기화</button>
+      <div>
+        입력값 : test
+      </div>
+    </div>
   )
 }
 
